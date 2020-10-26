9249f72ef65601855e9a377d88e28cdd9b2b6bf3
Ratheesh Kannan <ratheesh.kannan@tavant.com>
2020-10-25 20:57:23 -0700

    Updates

---
+++ precache-manifest.8248cd46eb989001a2f81b1cd8fc642d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e29fd5e595ab2b8cabcafdad2bc126",
+    "url": "/item-editor/index.html"
+  },
+  {
+    "revision": "a5829556566611df3fba",
+    "url": "/item-editor/static/css/main.db231fd3.chunk.css"
+  },
+  {
+    "revision": "546631a93a8ab6a27b22",
+    "url": "/item-editor/static/js/2.364efa99.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/item-editor/static/js/2.364efa99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5829556566611df3fba",
+    "url": "/item-editor/static/js/main.0cdeaf04.chunk.js"
+  },
+  {
+    "revision": "c7ff5fd92d29f7c3aa7b",
+    "url": "/item-editor/static/js/runtime-main.9ed6122f.js"
+  }
+]);
